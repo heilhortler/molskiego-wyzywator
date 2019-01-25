40bc681b3473b6a2c7e1dce97e85f1402ca2ca96
cdk <notcdk@protonmail.com>
2019-01-25 21:31:07 +0100

    update xd

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const scheduler = require('node-schedule');
+"use strict";
+
+const discord = require("discord.js");
+const scheduler = require("node-schedule");
+const cfg = require("./config.json");
+const discordClient = new discord.Client();
+const insultsCount = Object.keys(cfg.insults).length;
 
 var sterowniaBota;
 var ogolnyKanal;
+var molski;
 
-client.on('ready', () => {
-  sterowniaBota = client.channels.find(ch => ch.name === 'sterownia-bota');
-  ogolnyKanal = client.channels.find(ch => ch.name === 'ogólny');
+function randomInsult() {
+    let num = Math.round(Math.random() * Math.floor(insultsCount - 1));
+    return cfg.insults[num];
+}
 
-  if(!sterowniaBota || !ogolnyKanal)
-  {
-    console.log('kurwa');
+discordClient.on("ready", () => {
+  sterowniaBota = discordClient.channels.find(
+    ch => ch.name === "sterownia-bota"
+  );
+  ogolnyKanal = discordClient.channels.find(
+      ch => ch.name === "ogólny"
+  );
+  molski = discordClient.users.find(
+      u => u.id === "386462655189221376"
+  );
+
+  if (!sterowniaBota || !ogolnyKanal || !molski) {
+    console.warn("o chuj xddd");
     process.exit(-1);
   }
-  sterowniaBota.send('cześć siema');
+  sterowniaBota.send("cześć siema");
 });
 
-client.on('message', msg => {
-  if(msg.channel.name === 'sterownia-bota' && msg.content === 'wyzywaj')
-  {
-    sterowniaBota.send('sie robi szefunciu');
-    ogolnyKanal.send('jebac cie molski cwelu kurwa');
+discordClient.on("message", msg => {
+  if (msg.author.id === molski.id)
+    msg.reply(randomInsult());
+  else {
+    if (msg.channel.name === "sterownia-bota") {
+      switch (msg.content) {
+        case "wyzywaj":
+          sterowniaBota.send("sie robi szefunciu");
+          ogolnyKanal.send(molski.toString() + " " + randomInsult());
+          break;
+        default:
+          break;
+      }
+    }
   }
 });
 
-client.login(process.env.token);
+discordClient.login(process.env.token);
 
-scheduler.scheduleJob('20 16 * * *', function(){
-    ogolnyKanal.send('4:20 CHŁOPCY PALIMY!!!!!!!!!')
+scheduler.scheduleJob("20 16 * * *", function() {
+  ogolnyKanal.send("4:20 CHŁOPCY PALIMY!!!!!!!!!");
 });
