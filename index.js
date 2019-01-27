b446995a89a4f8a7213415f8f55311a3b6cfbcca
cdk <notcdk@protonmail.com>
2019-01-27 21:26:40 +0100

    nowosci

--- index.js
+++ index.js
@@ -1,57 +1,73 @@
-"use strict";
+'use strict';
 
-const discord = require("discord.js");
-const scheduler = require("node-schedule");
-const cfg = require("./config.json");
+const discord = require('discord.js');
+const schedule = require('node-schedule');
+const isNumber = require('is-number');
+const cfg = require('./config.json');
+
 const discordClient = new discord.Client();
-const insultsCount = Object.keys(cfg.insults).length;
+const insultsCount = Object.keys(cfg.insults).length; // liczba obelg
+const czterdwazeroCount = Object.keys(cfg.fourtwenty).length; // :)
+const zamknijmordeCount = Object.keys(cfg.shutup).length; // itd
 
-var sterowniaBota;
 var ogolnyKanal;
 var molski;
 
-function randomInsult() {
-    let num = Math.round(Math.random() * Math.floor(insultsCount - 1));
-    return cfg.insults[num];
+function randomInt(max) {
+  return Math.round(Math.random() * Math.floor(max)); // generuje losowa liczbe calkowita z przedzialu 0-max
 }
 
-discordClient.on("ready", () => {
-  sterowniaBota = discordClient.channels.find(
-    ch => ch.name === "sterownia-bota"
-  );
-  ogolnyKanal = discordClient.channels.find(
-      ch => ch.name === "ogólny"
-  );
-  molski = discordClient.users.find(
-      u => u.id === "386462655189221376"
-  );
+discordClient.on('ready', () => {
+  ogolnyKanal = discordClient.channels.find(ch => ch.name === 'ogólny'); // szuka ogolnego
+  molski = discordClient.users.find(u => u.id === '386462655189221376'); // szuka molskiego
 
-  if (!sterowniaBota || !ogolnyKanal || !molski) {
-    console.warn("o chuj xddd");
+  if (!ogolnyKanal || !molski) {
+    console.warn('o chuj xddd'); // jak nie znajdzie to przypix
     process.exit(-1);
   }
-  sterowniaBota.send("cześć siema");
 });
 
-discordClient.on("message", msg => {
-  if (msg.author.id === molski.id)
-    msg.reply(randomInsult());
-  else {
-    if (msg.channel.name === "sterownia-bota") {
-      switch (msg.content) {
-        case "wyzywaj":
-          sterowniaBota.send("sie robi szefunciu");
-          ogolnyKanal.send(molski.toString() + " " + randomInsult());
-          break;
-        default:
-          break;
+discordClient.on('message', msg => {
+  if (msg.author.id === molski.id) {
+    if (msg.content !== 'wyzywaj') {
+      // nie robimy to czego chce molski
+      msg.reply(cfg.shutup[randomInt(zamknijmordeCount - 1)]); // odpowiada molskiemu jak cos napisze
+    }
+  } else {
+    // ktos inny niz molski
+    if (!msg.content.startsWith('wyzywaj') || msg.author.bot) return;
+
+    if (msg.content === 'wyzywaj') {
+      ogolnyKanal.send(
+        molski.toString() + ' ' + cfg.insults[randomInt(insultsCount - 1)] // bot wyzywa na ogolnym
+      );
+    } else {
+      let cmd = msg.content.split(/ +/);
+      if (isNumber(cmd[1])) {
+        let num = parseInt(cmd[1]);
+        if (num < 0) return;
+        for (var i = 0; i < num; i++) {
+          setTimeout(() => {
+            ogolnyKanal.send(
+              molski.toString() + ' ' + cfg.insults[randomInt(insultsCount - 1)]
+            );
+          }, 500 * i);
+        }
       }
     }
   }
 });
 
-discordClient.login(process.env.token);
+discordClient.login(process.env.token); // tajne info
 
-scheduler.scheduleJob("20 16 * * *", function() {
-  ogolnyKanal.send("4:20 CHŁOPCY PALIMY!!!!!!!!!");
+// wyzywa molskiego co 45 minut
+schedule.scheduleJob('* /45 * * * *', () => {
+  ogolnyKanal.send(
+    molski.toString() + ' ' + cfg.insults[randomInt(insultsCount - 1)]
+  );
+});
+
+// :)
+schedule.scheduleJob('0 20 16 * * *', () => {
+  ogolnyKanal.send(cfg.fourtwenty[randomInt(czterdwazeroCount - 1)]);
 });
